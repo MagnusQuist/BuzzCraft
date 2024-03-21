454fced42a7db404a37f96ae58f2fac11eb4266e
Magnus Quist <cloudquist@gmail.com>
2024-03-21 16:41:28 +0100

    initial commit

---
+++ src/main/java/dk/buzzcraft/BuzzCraft.java
@@ -0,0 +1,17 @@
+package dk.buzzcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BuzzCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
